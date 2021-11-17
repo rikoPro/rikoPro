fe2ee469d5e4bac0c148f767c0e14e5a7f4b5638
rikoPro <atalayevrim.ea@gmail.com>
2021-11-17 19:43:43 +0300

    mcsd
    mcsd

---
+++ denemeler/Solution1/ConsoleApp1/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Calculator
+    {
+        public double Topla(double number1, double number2)
+        {
+            double sonuc = number1 + number2;
+            return sonuc;
+        }
+        public double Cikar(double number1, double number2)
+        {
+            double sonuc = number1 - number2;
+            return sonuc;
+        }
+        public double Carp(double number1, double number2)
+        {
+            double sonuc = number1 * number2;
+            return sonuc;
+        }
+        public double Bol(double number1, double number2)
+        {
+            double sonuc = number1 / number2;
+            return sonuc;
+        }
+    }
+}
---
+++ denemeler/Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Sayıyı Giriniz : ");
+            string n1 = Console.ReadLine();
+            
+            double number1 = Convert.ToDouble(n1);
+            Type t = number1.GetType();
+            if (t.Equals(typeof(double)))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Lütfen bir sayı giriniz!");
+            }
+            
+            {
+
+            }
+
+        }
+    }
+}
